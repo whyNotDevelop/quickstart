ad60253562b705f336cf364861c262338cc04007
whyNotDevelop <sicelothusi91@gmail.com>
2026-08-14 05:40:00 +0200

    Initialize SpringBoot application

---
+++ src/test/java/com/whyNotDevelop/quickstart/QuickstartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whyNotDevelop.quickstart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickstartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
